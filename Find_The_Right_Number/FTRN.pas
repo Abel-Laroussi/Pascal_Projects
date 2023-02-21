e9b5f0538db9ffcb2f979471047c33e8a6510a58
Abel-Laroussi <laroussi.abel@hotmail.fr>
2023-02-21 13:07:45 +0100

    Variable / Constants declarations

---
+++ Find_The_Right_Number/FTRN.pas
@@ -0,0 +1,11 @@
+program Find_The_Right_Number;                                  { Program's name }
+
+var to_guess, user_guess : integer;                             { Variables declaration }
+
+const                                                           { Constants declaration }
+more = 'The number to find is greater than your proposition';
+less = 'The number to find is less than your proposition';
+
+begin                                                           { Main loop begin }
+    
+end.
---
+++ Find_The_Right_Number/README.md
@@ -0,0 +1,17 @@
+### Find The Right Number
+
+Pascal challenging you to find the number it has chosen
+
+- [x] Declare variables / const
+
+- [ ] Main Loop
+
+- [ ] Display "more" or "less" messages
+
+- [ ] Generate random number in a function
+
+- [ ] Handle exceptions
+
+---
+
+*Produced by Abel Laroussi*
