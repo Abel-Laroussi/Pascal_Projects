21afc1b26c050a0745fcf1691745ee71e23949b7
Abel-Laroussi <laroussi.abel@hotmail.fr>
2023-02-21 12:38:46 +0100

    Variable and Constant declarations

---
+++ Hello_World/Hello_World.pas
@@ -0,0 +1,9 @@
+program Hello_World;                                { Name of the Program }
+
+const msg = 'Hello ! I hope you are doing fine ';   { Constants declarations }
+
+var username: string;                               { Variable declarations }
+
+begin
+    
+end.
